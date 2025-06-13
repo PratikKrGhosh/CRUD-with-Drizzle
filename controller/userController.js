d11e8e443d88a808edc857b032cdf7c28e8c0b4a
PratikKrGhosh <work.pratikkrghosh@gmail.com>
2025-06-14 02:04:53 +0530

    Created basic structure of the project and the insertion function

---
+++ controller/userController.js
@@ -0,0 +1,32 @@
+import db from "../config/db.js";
+import { usersTable } from "../drizzle/schema.js";
+
+export const createUser = async (req, res) => {
+  try {
+    const newUser = await db.insert(usersTable).values(req.body);
+    return res.status(201).send(newUser);
+  } catch (err) {
+    return res.status(400).send("Something Went Wrong");
+  }
+};
+
+export const getUser = async (req, res) => {
+  try {
+  } catch (err) {
+    return res.status(400).send("Something Went Wrong");
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+  } catch (err) {
+    return res.status(400).send("Something Went Wrong");
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+  } catch (err) {
+    return res.status(400).send("Something Went Wrong");
+  }
+};
